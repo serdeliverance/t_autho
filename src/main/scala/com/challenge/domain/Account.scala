9b6df9d3ed74c0aa9e70cf3079df867f81e5f329
Sergio Cano <canosergio90@gmail.com>
2021-10-25 23:22:04 -0300

    account balance test

--- src/main/scala/com/challenge/domain/Account.scala
+++ src/main/scala/com/challenge/domain/Account.scala
@@ -2,4 +2,6 @@ package com.challenge.domain
 
 case class Account(activeCard: Boolean, availableLimit: Int, transactions: List[Transaction]) {
   def process(transaction: Transaction): Account = this.copy(transactions = transaction :: this.transactions)
+
+  def balance(): Int = transactions.map(_.amount).fold(availableLimit)(_ - _)
 }
--- src/test/scala/com/challenge/domain/AccountSpec.scala
+++ src/test/scala/com/challenge/domain/AccountSpec.scala
@@ -1,5 +1,24 @@
 package com.challenge.domain
 
-class AccountSpec {
-  // TODO account creation
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.funsuite.AnyFunSuite
+
+import java.time.LocalDateTime
+
+class AccountSpec extends AnyFunSuite {
+  // TODO process transaction
+
+  // TODO account balance
+  test("calculate balance correctly") {
+    val transactions = List(
+      Transaction("burger king", 10, LocalDateTime.now().minusDays(2)),
+      Transaction("Mc Donalds", 20, LocalDateTime.now().minusDays(2))
+    )
+
+    val account = Account(true, 100, transactions)
+
+    val result = account.balance()
+
+    assertResult(70)(result)
+  }
 }
