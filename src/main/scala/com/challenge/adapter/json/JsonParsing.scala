c4b4cc6173a0894bd97fabf6f248fd222647b321
Sergio Cano <canosergio90@gmail.com>
2021-10-28 14:12:22 -0300

    operation result refactor
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/main/scala/com/challenge/adapter/json/JsonParsing.scala
+++ src/main/scala/com/challenge/adapter/json/JsonParsing.scala
@@ -2,7 +2,7 @@ package com.challenge.adapter.json
 
 import cats.syntax.functor._
 import com.challenge.adapter.json.input.{AuthorizeTransactionJson, CreateAccountJson}
-import com.challenge.domain.entity.OperationResult
+import com.challenge.domain.entity.{AccountBalance, OperationResult}
 import io.circe.{Decoder, Encoder}
 import io.circe.generic.auto._
 import io.circe.generic.extras.semiauto.deriveConfiguredEncoder
@@ -10,6 +10,8 @@ import io.circe.generic.extras.semiauto.deriveConfiguredEncoder
 object JsonParsing extends CirceImplicits {
 
   implicit val operationResultEncoder: Encoder[OperationResult] = deriveConfiguredEncoder
+
+  implicit val accountBalanceEncoder: Encoder[AccountBalance] = deriveConfiguredEncoder
 
   implicit val commandDecoder: Decoder[JsonInput] =
     List[Decoder[JsonInput]](
